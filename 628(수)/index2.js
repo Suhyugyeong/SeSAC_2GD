98dfc314e1fcb50ccc0b3d0e620894b4bca6873c
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-06-28 20:04:36 +0900

    628f

--- 628(수)/index2.js
+++ 628(수)/index2.js
@@ -3,10 +3,13 @@
 // mathScore = Number(mathScore);
 // engScore = Number(engScore);
 // let avg = (mathScore + engScore) / 2;
-
 // console.log(avg);
 
 console.log(`"${typeof 123}" isn't "${typeof "문자"}" data type.`);
 console.log(
   `typeof를 oolean이나 null에 사용하면, "${typeof null}" 결과를 얻을 수 있습니다.`
 );
+
+console.log(typeof 123 + " isn't " + typeof 문자 + " data type.");
+
+console.log(typeof 123, "isn't", typeof 문자, "data type.");
