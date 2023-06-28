c4142504201df68f920d742188747fe08107806f
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-06-28 17:21:53 +0900

    628le

---
+++ 628(수)/index2.js
@@ -0,0 +1,12 @@
+// let mathScore = prompt("수학점수를 입력하세요");
+// let engScore = prompt("영어점수를 입력하세요");
+// mathScore = Number(mathScore);
+// engScore = Number(engScore);
+// let avg = (mathScore + engScore) / 2;
+
+// console.log(avg);
+
+console.log(`"${typeof 123}" isn't "${typeof "문자"}" data type.`);
+console.log(
+  `typeof를 oolean이나 null에 사용하면, "${typeof null}" 결과를 얻을 수 있습니다.`
+);
