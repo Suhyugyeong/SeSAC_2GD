73dac92b0f88ae041fd87c45ef472d7651c7afc9
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-07-06 12:56:12 +0900

    0705f

--- 0705(수)/이벤트.js
+++ 0705(수)/이벤트.js
@@ -3,17 +3,18 @@ const btn2 = document.querySelector(".btn--green");
 const btn3 = document.querySelector(".btn--blue");
 const btn4 = document.querySelector(".btn--red");
 
+/* btn1 */
 btn1.addEventListener("click", function () {
-  alert("버튼1을 클릭했군요");
+  alert("버튼 1을 클릭하셨군요!");
 });
 
-btn2.addEventListener("mouseover", function () {
-  btn2.style.backgroundColor = "gray";
+btn1.addEventListener("mouseover", function () {
+  btn1.style.backgroundColor = "gray";
 });
 
+/* btn2 */
 const container = document.getElementById("container");
 
-console.log(contrainer);
 btn2.addEventListener("click", () => {
   const div = document.createElement("div");
   div.innerText = "안녕";
@@ -21,25 +22,30 @@ btn2.addEventListener("click", () => {
   container.append(div);
 });
 
-
-function changeColor("click", function (){
-    const divs = document.querySelectorAll('#container div');
-    for(let div of divs);}
-div.style.backgroundColor = "skyblue";
-div[divs.length - 1].style.backgroundColor = "red";
-)
-//divs가 배열임 div는 for문 쓰기 위해 만들어준...
-
-btn3.addEventListener('click', changeColor)
-
-function changeBtnColor(){
-    this.style.backgroundColor = "yellow";
-    this.style.backgroundColor = "red";
+/* btn3 */
+function changeColor() {
+  const divs = document.querySelectorAll("#container div");
+  for (let div of divs) {
+    div.style.backgroundColor = "skyblue";
+  }
+  //   맨 마지막 요소만 빨강색으로 변경!
+  divs[divs.length - 1].style.backgroundColor = "red";
 }
-//이벤트 적용할때 btn4 적용하면 btn4만 적용되기 때문에 
-//이럴때는 this를 이용해서 자기값에 반응할 수 있도록해야함
 
+btn3.addEventListener("click", changeColor);
+
+/* btn4 */
+function changeBtnColor() {
+  this.style.backgroundColor = "yellow";
+  this.style.color = "#000";
+}
 btn4.addEventListener("click", changeBtnColor);
 
+/* click 외의 많은 동작들 */
 console.log(window);
-window.addEventListener('scroll')
+window.addEventListener("scroll", () => {
+  console.log(scrollY);
+  if (scrollY > 900) {
+    document.querySelector(".pos").style.opacity = "1";
+  }
+});
