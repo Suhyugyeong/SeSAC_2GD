d8d0e05900bed09487b21dc447881dfa0557e599
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-09-08 21:29:02 +0900

    0908new

---
+++ 노마드코더/nomad.js
@@ -0,0 +1,16 @@
+const a = 5;
+const b = 2;
+
+let name = "yugyeong";
+
+console.log(5 % 2);
+console.log(a % 2);
+console.log(a % b);
+console.log("your the best, " + name);
+
+name = "suhyugyeong!";
+console.log(name + "Do the best!");
+
+a = 4;
+//const 변경 불가능
+console.log(a % 2);
