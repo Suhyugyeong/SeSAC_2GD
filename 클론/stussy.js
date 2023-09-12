337a196c6752ea147fc11aa334b4b6b603353d54
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-09-12 20:03:22 +0900

    0912addtojs

---
+++ 클론/stussy.js
@@ -0,0 +1,9 @@
+document.getElementById("instagram").addEventListener("click", function () {
+  // Google 홈페이지를 새 탭에서 열기 위해 window.open() 메서드를 사용합니다.
+  window.open("https://www.instagram.com/stussy/", "_blank");
+});
+
+document.getElementById("vimeo").addEventListener("click", function () {
+  // Google 홈페이지를 새 탭에서 열기 위해 window.open() 메서드를 사용합니다.
+  window.open("https://vimeo.com/stussy", "_blank");
+});
