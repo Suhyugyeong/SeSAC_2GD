d82231e88e361eccb10a823b3d34c5616ec8f686
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-09-13 05:00:33 +0900

    0913clone

--- 클론/stussy.js
+++ 클론/stussy.js
@@ -7,10 +7,6 @@
 //   window.open("https://vimeo.com/stussy", "_blank");
 // });
 
-// document.querySelector(".snsicon i").addEventListener("click", function () {
-//   window.open("https://vimeo.com/stussy", "_blank");
-// });
-
 // 첫 번째 <i> 태그에 이벤트 리스너를 추가하고 다른 사이트로 이동합니다.
 document.querySelector(".fa-instagram").addEventListener("click", function () {
   window.open("https://www.instagram.com/stussy/", "_blank");
@@ -19,27 +15,4 @@ document.querySelector(".fa-instagram").addEventListener("click", function () {
 // 두 번째 <i> 태그에 이벤트 리스너를 추가하고 다른 사이트로 이동합니다.
 document.querySelector(".fa-vimeo-v").addEventListener("click", function () {
   window.open("https://vimeo.com/stussy", "_blank");
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const addDiv1 = document.getElementsByClassName("addDiv1");
-
-  addDiv1.addEventListener("click", function (event) {
-    if (event.target.classList.contains("addDiv2")) {
-      // 새로운 div 생성
-      const newDiv = document.createElement("div");
-      newDiv.textContent = "새로운 div가 생성되었습니다!";
-
-      // 생성한 div에 스타일 추가 (예: 배경색)
-      newDiv.style.backgroundColor = "lightblue";
-
-      // 생성한 div를 container에 추가
-      addDiv1.appendChild(newDiv);
-
-      // 새로운 div를 클릭할 때의 동작을 추가 (생략 가능)
-      newDiv.addEventListener("click", function () {
-        alert("새로운 div를 클릭했습니다!");
-      });
-    }
-  });
 });
